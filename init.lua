469be20c6ddc4bbde674098decb2626a91948309
teschel6 <elijah.tesch@gmail.com>
2025-05-25 20:14:14 -0600

    spaces > tabs

--- init.lua
+++ init.lua
@@ -12,6 +12,12 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Use spaces when <tab> is pressed
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 -- Make line numbers default
 vim.o.number = true
 
